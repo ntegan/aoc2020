3184eb53f619660b0d20a58d9ed338df474a999c
ntegan <ntegan1@gmail.com>
2020-12-02 09:26:06 -0600

    day 2.1

--- src/main.rs
+++ src/main.rs
@@ -16,29 +16,109 @@ mod myerror;
 // =======
 // find 3 numbers that meet the same criteria?
 //
+// Day 2.2
+// =======
+// Need t oget to coastal airport. quickest way is via toboggan.
+// North Pole Toboggan Rental Shop owner has bad day, can't log into computers.
+// password database corupted.
+//
+// debug problem given list of passwords (puzzle input) according to corrupted
+// database and corporate policy when that password was set
+//
+// e.g.
+// 1-3 a: abcde
+// 1-3 b: cdefg
+// 2-9 c: ccccccccc
+//
+// policy indicates lowest and highest frequency a letter must appear for pass
+// to be valid.
+// 1-3 a means password must contain a [1,3] times.
+// above example 2 passwords are valid.
+
+mod authentication {
+    use regex::Regex;
+    use std::fmt;
+
+    #[derive(Debug)]
+    pub struct Policy {
+        start: u64,
+        end: u64,
+        letter: char,
+    }
+
+    impl fmt::Display for Policy {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "{}-{} {}", self.start, self.end, self.letter)
+        }
+    }
+
+    #[derive(Debug)]
+    pub struct Entry {
+        policy: Policy,
+        password: String,
+    }
+    impl fmt::Display for Entry {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "{}: {}", self.policy, self.password)
+        }
+    }
+
+    impl Entry {
+        /// Ensure the frequency of self.policy.letter in self.password
+        /// is in [self.policy.start, self.policy.ent]
+        pub fn is_valid(&self) -> bool {
+            let count = self.password.matches(self.policy.letter).count() as u64;
+            std::ops::Range {
+                start: self.policy.start,
+                end: self.policy.end + 1,
+            }
+            .contains(&count)
+        }
+    }
+
+    pub fn parse_line_to_entry(line: &str) -> Result<Entry, Box<dyn std::error::Error>> {
+        let re = Regex::new("^([0-9]*)-([0-9]*) ([a-zA-Z]): ([a-zA-Z]*)$")?;
+        let caps = re.captures(line).ok_or("Couldn't get captures")?;
+
+        let start = caps
+            .get(1)
+            .ok_or("Couldn't get capture group")?
+            .as_str()
+            .parse::<u64>()?;
+        let end = caps
+            .get(2)
+            .ok_or("Couldn't get capture group")?
+            .as_str()
+            .parse::<u64>()?;
+        let letter = caps
+            .get(3)
+            .ok_or("Couldn't get capture group")?
+            .as_str()
+            .parse::<char>()?;
+        let password = caps.get(4).ok_or("Couldn't get capture group")?.as_str();
+
+        Ok(Entry {
+            policy: Policy { start, end, letter },
+            password: password.into(),
+        })
+    }
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let input = input::read_until_eof()?;
 
-    let expense_report_entries = input
+    let entries = input
         .lines()
-        .map(|line| line.parse::<u64>())
-        .collect::<Result<Vec<u64>, _>>()?;
+        .map(authentication::parse_line_to_entry)
+        .collect::<Result<Vec<authentication::Entry>, _>>()?;
+    let valid_entries: Vec<&authentication::Entry> =
+        entries.iter().filter(|entry| entry.is_valid()).collect();
 
-    for i in 0..expense_report_entries.len() {
-        for j in 0..expense_report_entries.len() {
-            for k in 0..expense_report_entries.len() {
-                let a = expense_report_entries[i];
-                let b = expense_report_entries[j];
-                let c = expense_report_entries[k];
-                if a + b + c == 2020 {
-                    println!("{} + {} + {} == 2020", a, b, c);
-                    println!("{} * {} * {} == {}", a, b, c, a * b * c);
-                    return Ok(());
-                }
-            }
-        }
+    //println!("{}", entries);
+    for entry in &entries {
+        println!("{}", entry.to_string());
     }
+    println!("Valid entries: {} / {}", valid_entries.len(), entries.len());
 
     Err(Box::new(myerror::MyError))
 }
